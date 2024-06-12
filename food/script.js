3fad80fabaa42058a66aada0a34eeb5c47651bb4
Liso2004 <lisowycliff@gmail.com>
2024-06-12 21:04:33 +0200

    Add files via upload

---
+++ food/script.js
@@ -0,0 +1,10 @@
+Js
+function order() {
+    alert("Your order has been placed!");
+}
+
+function submitForm(event) {
+    event.preventDefault();
+    // Code to handle form submission (e.g., send data to server)
+    alert("Form submitted successfully!");
+}
