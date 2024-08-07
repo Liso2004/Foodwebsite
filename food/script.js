52ba686807ddc30b665ef35c33f611f099cca0d0
Liso W.S hlatshwayo <lisowycliff@gmail.com>
2024-08-07 21:31:24 +0200

    added more sections and css icons

--- food/script.js
+++ food/script.js
@@ -3,10 +3,6 @@ function order() {
     alert("Your order has been placed!");
 }
 
-function onlineorder () {
-    
-}
-
 function submitForm(event) {
     event.preventDefault();
     // Code to handle form submission (e.g., send data to server)
