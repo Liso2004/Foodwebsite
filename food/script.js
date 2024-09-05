1f65fd2e4fe1478390c3577ad4fb5615a5a248f3
Liso W.S hlatshwayo <lisowycliff@gmail.com>
2024-09-05 18:58:38 +0200

    added fire base and made a evn file for database details

--- food/script.js
+++ food/script.js
@@ -1,20 +1,24 @@
 // Import the Firebase JavaScript SDK
 import firebase from 'firebase/app';
-import 'firebase/database';
+import { getDatabase } from "firebase/database";
+import dotenv from 'dotenv';
+
+dotenv.config();
+
 
 // Initialize the Firebase app
-firebase.initializeApp({
-  apiKey: '<API_KEY>',
-  authDomain: '<AUTH_DOMAIN>',
-  databaseURL: '<DATABASE_URL>',
-  projectId: '<PROJECT_ID>',
-  storageBucket: '<STORAGE_BUCKET>',
-  messagingSenderId: '<MESSAGING_SENDER_ID>'
-});
-
+// get keys from env file 
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+};
 // Get a reference to the Realtime Database
-const db = firebase.database();
-
+const app = initializeApp(firebaseConfig);
 // Define the restaurant locations collection
 const restaurantLocationsRef = db.ref('restaurantLocations');
 
